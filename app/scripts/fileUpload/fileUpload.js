d48867db642f7ce4f5d3f26ecd6e0da7ddd09970
Jose Manuel Rosa <josemanuel.rosamoncayo@gmail.com>
2015-11-04 01:30:14 +0100

    Reove credentials

--- app/scripts/fileUpload/fileUpload.js
+++ app/scripts/fileUpload/fileUpload.js
@@ -6,8 +6,8 @@
     .constant('WISTIA', {
         url: 'https://upload.wistia.com',
         media: 'https://api.wistia.com/v1/medias/',
-        project: 'dpwy1v0dgg',
-        token: '4031fd8e9c92028fcff69709ad35000bc1d8fbe6deeb50032f2824e16ae705e8',
+        project: '',
+        token: '',
     });
 
 })();
